78ae16293fb9ed71868e3183a0dd2d93113e70bf
riyan-eng <fbriyant11@gmail.com>
2023-01-13 18:10:23 +0700

    list class

--- controllers/classController.go
+++ controllers/classController.go
@@ -5,6 +5,7 @@ import (
 	"github.com/google/uuid"
 	"github.com/riyan-eng/api-praxis-online-class/helpers"
 	"github.com/riyan-eng/api-praxis-online-class/models"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 func CreateClass(c *fiber.Ctx) error {
@@ -28,15 +29,7 @@ func CreateClass(c *fiber.Ctx) error {
 		})
 	}
 
-	// insert to database
-	// collection := models.ClassCollection()
-	// collection.Indexes().CreateOne(
-	// 	context.Background(),
-	// 	mongo.IndexModel{
-	// 		Keys:    bson.D{{Key: "class_code", Value: 1}},
-	// 		Options: options.Index().SetUnique(true),
-	// 	},
-	// )
+	// access to database
 	result, err := models.ClassCollection().InsertOne(c.Context(), class)
 	if err != nil {
 		return c.Status(fiber.StatusBadGateway).JSON(fiber.Map{
@@ -51,8 +44,29 @@ func CreateClass(c *fiber.Ctx) error {
 }
 
 func ReadClasses(c *fiber.Ctx) error {
+	result, err := models.ClassCollection().Find(c.Context(), bson.M{})
+	if err != nil {
+		return c.Status(fiber.StatusBadGateway).JSON(fiber.Map{
+			"data":    err.Error(),
+			"message": "fail",
+		})
+	}
+	defer result.Close(c.Context())
+
+	var classes []models.Class
+	for result.Next(c.Context()) {
+		var class models.Class
+		if err = result.Decode(&class); err != nil {
+			return c.Status(fiber.StatusBadGateway).JSON(fiber.Map{
+				"data":    err.Error(),
+				"message": "fail",
+			})
+		}
+		classes = append(classes, class)
+	}
+
 	return c.Status(fiber.StatusOK).JSON(fiber.Map{
-		"data":    "list",
+		"data":    classes,
 		"message": "ok",
 	})
 }
--- models/classModel.go
+++ models/classModel.go
@@ -6,11 +6,11 @@ import (
 )
 
 type Class struct {
-	ID              string  `bson:"_id"`
+	ID              string  `json:"id" bson:"_id"`
 	ClassName       string  `json:"class_name" bson:"class_name" validate:"required"`
 	ClassCode       string  `json:"class_code" bson:"class_code" validate:"required"`
 	ClassMonthPrice float32 `json:"class_month_price" bson:"class_month_price" validate:"required"`
-	IsActive        bool    `bson:"is_active"`
+	IsActive        bool    `json:"is_active" bson:"is_active"`
 }
 
 func ClassCollection() *mongo.Collection {
