d33c47469fae4679dc45172f2b738160a1775a24
riyan-eng <fbriyant11@gmail.com>
2023-01-13 10:12:25 +0700

    fiber

--- main.go
+++ main.go
@@ -1,16 +1,58 @@
 package main
 
 import (
-	"fmt"
+	"os"
 
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/recover"
 	"github.com/riyan-eng/api-praxis-online-class/initializers"
 )
 
 func init() {
 	initializers.LoadEnvVariable()
-	initializers.ConnectToDatabase()
 }
 
 func main() {
-	fmt.Println("hello riyan")
+	err := run()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func run() error {
+	// init db
+	err := initializers.ConnectToDatabase()
+	if err != nil {
+		return err
+	}
+
+	// defer closing db
+	defer initializers.CloseDatabase()
+	app := fiber.New()
+
+	// middleware
+	app.Use(logger.New())
+	app.Use(recover.New())
+	app.Use(cors.New())
+
+	// metrics
+	// app.Get("/metrics", monitor.New(monitor.Config{Title: "MyService Metrics Page"}))
+
+	// route
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World 👋!")
+	})
+	app.Get("/a", func(c *fiber.Ctx) error {
+		panic("I'm an error")
+	})
+
+	// start server
+	var port string
+	if port = os.Getenv("SERVER_PORT"); port == "" {
+		port = "8080"
+	}
+	app.Listen(":" + port)
+	return nil
 }
--- models/classModel.go
+++ models/classModel.go
@@ -1,1 +1,9 @@
 package models
+
+type Class struct {
+	ID              string `json:"id" bson:"_id"`
+	ClassName       string `json:"class_name" bson:"class_name"`
+	ClassCode       string `json:"class_code" bson:"class_code"`
+	ClassMonthPrice int    `json:"class_month_price" bson:"class_month_price"`
+	IsActive        bool   `json:"is_active" bson:"is_active"`
+}
