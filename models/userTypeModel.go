7b3f92718f234cbb420dfbf3ac359a1d029e2a75
riyan-eng <fbriyant11@gmail.com>
2023-01-15 20:02:11 +0700

    models UserType

--- models/userTypeModel.go
+++ models/userTypeModel.go
@@ -1,1 +1,18 @@
 package models
+
+import (
+	"github.com/riyan-eng/api-praxis-online-class/initializers"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type UserType struct {
+	ID           string `json:"id" bson:"_id"`
+	UserTypeName string `json:"user_type_name" bson:"user_type_name" validate:"required"`
+	UserTypeCode string `json:"user_type_code" bson:"user_type_code" validate:"required"`
+	IsActive     bool   `json:"is_active" bson:"is_active"`
+}
+
+func UserTypeCollection() *mongo.Collection {
+	collection := initializers.Collection("user_types")
+	return collection
+}
