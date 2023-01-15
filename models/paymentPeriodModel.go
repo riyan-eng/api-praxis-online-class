96638888af02b6fe5721c4ca26b81de813508ad0
riyan-eng <fbriyant11@gmail.com>
2023-01-16 06:21:26 +0700

    models period

--- models/paymentPeriodModel.go
+++ models/paymentPeriodModel.go
@@ -1,9 +1,19 @@
 package models
 
+import (
+	"github.com/riyan-eng/api-praxis-online-class/initializers"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
 type PaymentPeriod struct {
 	ID         string  `bson:"_id" json:"id"`
 	PeriodCode string  `bson:"period_code" json:"period_code"`
 	PeriodName string  `bson:"period_name" json:"period_name"`
 	Discount   float32 `bson:"discount" json:"discount"`
 	IsActive   bool    `bson:"is_active" json:"is_active"`
+}
+
+func PaymentPeriodCollection() *mongo.Collection {
+	collection := initializers.Collection("payment_period")
+	return collection
 }
