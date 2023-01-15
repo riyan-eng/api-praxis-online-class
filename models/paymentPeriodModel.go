9c9c7497802963c4a8bd0415e3dff12b0219728f
riyan-eng <fbriyant11@gmail.com>
2023-01-16 06:19:22 +0700

    models period

--- models/paymentPeriodModel.go
+++ models/paymentPeriodModel.go
@@ -1,1 +1,9 @@
 package models
+
+type PaymentPeriod struct {
+	ID         string  `bson:"_id" json:"id"`
+	PeriodCode string  `bson:"period_code" json:"period_code"`
+	PeriodName string  `bson:"period_name" json:"period_name"`
+	Discount   float32 `bson:"discount" json:"discount"`
+	IsActive   bool    `bson:"is_active" json:"is_active"`
+}
