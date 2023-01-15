489326e1d4c14e0e14d22b072dd0822aa5677f93
riyan-eng <fbriyant11@gmail.com>
2023-01-15 20:06:49 +0700

    models controller create UserType

--- controllers/userTypeController.go
+++ controllers/userTypeController.go
@@ -1,1 +1,45 @@
 package controllers
+
+import (
+	"github.com/go-playground/validator/v10"
+	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
+	"github.com/riyan-eng/api-praxis-online-class/models"
+)
+
+var validate = validator.New()
+
+func CreateUserType(c *fiber.Ctx) error {
+	var userType models.UserType
+	userType.ID = uuid.New().String()
+	userType.IsActive = true
+
+	// validate require body json
+	if err := c.BodyParser(&userType); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"data":    err.Error(),
+			"message": "fail",
+		})
+	}
+
+	// validate body json
+	if errorValidate := validate.Struct(userType); errorValidate != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"data":    errorValidate,
+			"message": "fail",
+		})
+	}
+
+	// access to database
+	result, err := models.ClassCollection().InsertOne(c.Context(), userType)
+	if err != nil {
+		return c.Status(fiber.StatusBadGateway).JSON(fiber.Map{
+			"data":    err.Error(),
+			"message": "fail",
+		})
+	}
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"data":    result,
+		"message": "ok",
+	})
+}
